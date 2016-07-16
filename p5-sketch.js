ac1d076ff56342c827d4cdd3ab7f15db85734072
tanyuan <tanyuan@users.noreply.github.com>
2016-07-16 17:52:47 +0800

    Change to wave animation

--- p5-sketch.js
+++ p5-sketch.js
@@ -1,5 +1,6 @@
 var img;
 var color;
+var z = 0;
 
 function setup() {
     createCanvas(600, 600);
@@ -7,7 +8,18 @@ function setup() {
 }
 
 function draw() {
-    color = map(mouseY, 0, 600, 255, 0);
-    background(color);
+    amplitude = map(mouseX, 0, 600, 100, 300);
+    interval = map(mouseY, 0, 600, 100, 20);
+
+    background(255);
+    stroke(128, 100);
+
+    for (var y = 0; y < height; y += interval) {
+        for (var x = 0; x < width; x += 1) {
+            point(x, y + map(noise(x/150, y/150, z), 0, 1, -amplitude, amplitude));
+        }
+    }
+    z = z + 0.02;
+
     image(img, 0, 0);
 }
